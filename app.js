62f84eb46413368fae03ee2675166c2097501059
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2023-09-22 17:57:30 +0530

    Making GET Requests with the Node HTTPS Module

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const https = require("https");
 
 const app  = express();
 
-app.get("https://api.openweathermap.org/data/2.5/weather?q=London&appid=e0e6c07a1f5175be9ecc1bce398102d1&unit=matrix", function(req, res){
+app.get("/", function(req, res){
+  
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e0e6c07a1f5175be9ecc1bce398102d1&unit=matrix";
+
+  https.get(url, function(response){
+    console.log(response);
+  });
+
   res.send("Server is up and running.");
 });
 
