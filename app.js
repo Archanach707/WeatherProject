6e5174ebbdd60c8dbc8e651d68c4986d02925bef
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2023-09-22 18:42:37 +0530

    parse JSON

--- app.js
+++ app.js
@@ -8,10 +8,24 @@ app.get("/", function(req, res){
   const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e0e6c07a1f5175be9ecc1bce398102d1&unit=matrix";
 
   https.get(url, function(response){
-    console.log(response);
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      console.log(temp);
+
+      // const object = {
+      //   name: "Archana",
+      //   favouritePLACE: "Noida"
+      // }
+      // console.log(JSON.stringify(object));
+      
+    });
   });
 
   res.send("Server is up and running.");
+  
 });
 
 
