268233a7226d31f56a296969fb3034bf14ef6f0e
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2023-09-22 17:48:29 +0530

    make https requesr for makein get API

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const https = require("https");
 
 const app  = express();
 
-app.get("/", function(req, res){
+app.get("https://api.openweathermap.org/data/2.5/weather?q=London&appid=e0e6c07a1f5175be9ecc1bce398102d1&unit=matrix", function(req, res){
   res.send("Server is up and running.");
 });
 
 
 app.listen(4000, function() {
-  console.log("Server is running on post 4000.");
+  console.log("Server is running on port 4000.");
 });
