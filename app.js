7e601d81e9d116e1877f9a8feb2246e56ed2ba9c
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2023-09-23 17:15:15 +0530

    make dynamic URL data

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const https = require("https");
 const app  = express();
 
 app.get("/", function(req, res){
-  
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e0e6c07a1f5175be9ecc1bce398102d1&unit=matrix";
+ 
+  const query = "London";
+  const apiKey = "e0e6c07a1f5175be9ecc1bce398102d1";
+  const unit = "matrix";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey +"&unit=" + unit;
 
   https.get(url, function(response){
     console.log(response.statusCode);
@@ -15,7 +18,7 @@ app.get("/", function(req, res){
       const temp = weatherData.main.temp
       const weatherDescription = weatherData.weather[0].description
       const icon = weatherData.weather[0].icon
-      const imageURL = "https://openweathermap.org/img/wn/10d@2x.png";
+      const imageURL = "https://openweathermap.org/img/wn/10d@2x.png"
       res.write("<p>The weather is currently " + weatherDescription + "</p>");
       res.write("<h1>The temperature in London is " + temp + "degrees celcius.</h1>");
       res.write("<img src=" + imageURL +">");
