a512bc07aee0d733b6ca4dc47af4fb8f1992c08b
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2023-09-23 16:42:45 +0530

    by API URL added image

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ app.get("/", function(req, res){
       const weatherData = JSON.parse(data)
       const temp = weatherData.main.temp
       const weatherDescription = weatherData.weather[0].description
-      res.send("<h1>The temperature in London is " + temp + "degrees celcius.</h1>");
+      const icon = weatherData.weather[0].icon
+      const imageURL = "https://openweathermap.org/img/wn/10d@2x.png";
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write("<h1>The temperature in London is " + temp + "degrees celcius.</h1>");
+      res.write("<img src=" + imageURL +">");
+      res.send()
+
       
       // console.log(weatherDescription);
 
